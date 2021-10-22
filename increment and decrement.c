97b4d433aff520d03d0927762ef97bfe782f7c26
Migwi-Peterz <92159173+Migwi-Peterz@users.noreply.github.com>
2021-10-22 15:48:48 +0300

    Add files via upload

---
+++ increment and decrement.c
@@ -0,0 +1,9 @@
+/*increment and decrement operators*/
+#include <stdio.h>
+int main()
+{
+    int a = 10, b = 100;
+    printf("++a = %d \n", ++a);
+    printf("--b = %d \n", --b);
+    return 0;
+}
